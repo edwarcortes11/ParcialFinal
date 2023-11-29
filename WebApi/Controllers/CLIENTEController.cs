97ed7ebd3151b86cab1e146de9cdb13319ea1eac
Edwar Yesid Cortes Ocampo <edwar.cortes.0ds@utap.edu.co>
2023-11-28 21:01:08 -0500

    Agregar archivos de proyecto.

---
+++ WebApi/Controllers/CLIENTEController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ParcialFinal.Modelo.DB;
+
+namespace WebApi.Controllers
+{
+    public class CLIENTEController : ApiController
+    {
+        private DesarrolloWebParcialEntities db = new DesarrolloWebParcialEntities();
+
+        // GET: api/CLIENTE
+        public IQueryable<CLIENTE> GetCLIENTE()
+        {
+            return db.CLIENTE;
+        }
+
+        // GET: api/CLIENTE/5
+        [ResponseType(typeof(CLIENTE))]
+        public IHttpActionResult GetCLIENTE(int id)
+        {
+            CLIENTE cLIENTE = db.CLIENTE.Find(id);
+            if (cLIENTE == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(cLIENTE);
+        }
+
+        // PUT: api/CLIENTE/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCLIENTE(int id, CLIENTE cLIENTE)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != cLIENTE.IDCLIENTE)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(cLIENTE).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CLIENTEExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/CLIENTE
+        [ResponseType(typeof(CLIENTE))]
+        public IHttpActionResult PostCLIENTE(CLIENTE cLIENTE)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.CLIENTE.Add(cLIENTE);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (CLIENTEExists(cLIENTE.IDCLIENTE))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = cLIENTE.IDCLIENTE }, cLIENTE);
+        }
+
+        // DELETE: api/CLIENTE/5
+        [ResponseType(typeof(CLIENTE))]
+        public IHttpActionResult DeleteCLIENTE(int id)
+        {
+            CLIENTE cLIENTE = db.CLIENTE.Find(id);
+            if (cLIENTE == null)
+            {
+                return NotFound();
+            }
+
+            db.CLIENTE.Remove(cLIENTE);
+            db.SaveChanges();
+
+            return Ok(cLIENTE);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CLIENTEExists(int id)
+        {
+            return db.CLIENTE.Count(e => e.IDCLIENTE == id) > 0;
+        }
+    }
+}
---
+++ WebApi/Controllers/PRODUCTOController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using ParcialFinal.Modelo.DB;
+
+namespace WebApi.Controllers
+{
+    public class PRODUCTOController : ApiController
+    {
+        private DesarrolloWebParcialEntities db = new DesarrolloWebParcialEntities();
+
+        // GET: api/PRODUCTO
+        public IQueryable<PRODUCTO> GetPRODUCTO()
+        {
+            return db.PRODUCTO;
+        }
+
+        // GET: api/PRODUCTO/5
+        [ResponseType(typeof(PRODUCTO))]
+        public IHttpActionResult GetPRODUCTO(int id)
+        {
+            PRODUCTO pRODUCTO = db.PRODUCTO.Find(id);
+            if (pRODUCTO == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(pRODUCTO);
+        }
+
+        // PUT: api/PRODUCTO/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPRODUCTO(int id, PRODUCTO pRODUCTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != pRODUCTO.IDPRODUCTO)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(pRODUCTO).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PRODUCTOExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/PRODUCTO
+        [ResponseType(typeof(PRODUCTO))]
+        public IHttpActionResult PostPRODUCTO(PRODUCTO pRODUCTO)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.PRODUCTO.Add(pRODUCTO);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (PRODUCTOExists(pRODUCTO.IDPRODUCTO))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = pRODUCTO.IDPRODUCTO }, pRODUCTO);
+        }
+
+        // DELETE: api/PRODUCTO/5
+        [ResponseType(typeof(PRODUCTO))]
+        public IHttpActionResult DeletePRODUCTO(int id)
+        {
+            PRODUCTO pRODUCTO = db.PRODUCTO.Find(id);
+            if (pRODUCTO == null)
+            {
+                return NotFound();
+            }
+
+            db.PRODUCTO.Remove(pRODUCTO);
+            db.SaveChanges();
+
+            return Ok(pRODUCTO);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PRODUCTOExists(int id)
+        {
+            return db.PRODUCTO.Count(e => e.IDPRODUCTO == id) > 0;
+        }
+    }
+}
